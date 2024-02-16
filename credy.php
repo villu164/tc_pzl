277faeac47c5c9b9644c17239431ada005cf6c97
Villu Orav <998682+villu164@users.noreply.github.com>
2024-02-16 12:56:36 +0200

    Fix bio and technologies and remove redundant fields

--- credy.php
+++ credy.php
@@ -6,10 +6,8 @@ $data = [
     'first_name' => 'Villu',
     'last_name' => 'Orav',
     'email' => 'villu164@gmail.com',
-    'bio' => 'TODO:',
-    'technologies' => ['PHP', 'HTML', 'Docker'],
-    'timestamp' => $timestamp,
-    'signature' => $signature,
+    'bio' => "Oops, sorry for the TODO payload from earler. I'm a Developer my nature and I use coding as a tool to solve my problems. I'm also into security, bug bounties, CTF-s puzzles, technology, hardware, cloud... Well you could say that I'm quite nerdsnipeable. With the puzzle, I hope to show my capability to do research, PHP, debugging, source code reading, docker, OOP. I cannot really show MySQL/MariaDB knowledge, because my background has heavily been PostgreSQL, but as a core concept, I know how SQL in general works",
+    'technologies' => ['PHP', 'HTML', 'Docker', 'Ruby', 'PostgreSQL', 'ChatGPT/LLM'],
     'vcs_uri' => 'https://github.com/villu164/tc_pzl'
 ];
 
